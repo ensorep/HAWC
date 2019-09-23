da4a99bbfcfc762efb2778a92c9716813822e700
Reggie Peterson <tao.rep@gmail.com>
2019-09-22 23:07:09 -0400

    initial HAWC build

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,19 @@
 import React from "react"
-import { Link } from "gatsby"
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
-
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+export default () => (
+    <div id="app">
+        <header className="header">
+            <div className="header_branding">
+                <h1>HAWC</h1>
+                <p>Harnett Animal Welfare Coalition</p>
+            </div>
+            <nav className="header_nav">
+                <li className="header_nav-item">About</li>
+                <li className="header_nav-item">Get involved</li>
+                <li className="header_nav-item">Programs</li>
+                <li className="header_nav-item">Calendar</li>
+                <li className="header_nav-item">Donate</li>
+            </nav>
+        </header>
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
 )
-
-export default IndexPage
