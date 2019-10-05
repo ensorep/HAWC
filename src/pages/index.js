d49802b720d10e838b08dfbc539fae2980393c85
Reggie Peterson <tao.rep@gmail.com>
2019-10-04 23:11:46 -0400

    update basic styling & hover states

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,28 @@
 import React from "react"
 
+import { setConfig } from 'react-hot-loader'
+
+setConfig({
+    showReactDomPatchNotification: false
+})
 export default () => (
-    <div id="app">
-        <header className="header">
-            <div className="header_branding">
-                <h1>HAWC</h1>
-                <p>Harnett Animal Welfare Coalition</p>
-            </div>
-            <nav className="header_nav">
-                <li className="header_nav-item">About</li>
-                <li className="header_nav-item">Get involved</li>
-                <li className="header_nav-item">Programs</li>
-                <li className="header_nav-item">Calendar</li>
-                <li className="header_nav-item">Donate</li>
-            </nav>
-        </header>
+  <div id="app">
+    <header className="header">
+      <div className="header_branding">
+        <h1>HAWC</h1>
+        <p>Harnett Animal Welfare Coalition</p>
+      </div>
+      <nav className="header_nav">
+        <li className="header_nav-item">About</li>
+        <li className="header_nav-item">Get involved</li>
+        <li className="header_nav-item">Programs</li>
+        <li className="header_nav-item">Calendar</li>
+        <li className="header_nav-item donate">Donate</li>
+      </nav>
+    </header>
+    <div className="content">
+      <h2 className="content-tagline">Be A Hero</h2>
+      <button className="content_button-main">Get Involved</button>
     </div>
+  </div>
 )
